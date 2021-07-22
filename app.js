030259c553998795131829aa20fa6d06a40510df
Ayushi Verma <50084909+Ayushi20-19@users.noreply.github.com>
2021-07-22 12:48:15 +0530

    get input value in console

--- app.js
+++ app.js
@@ -5,13 +5,19 @@
 //querysector(i m asking u this thing) take the elemnt with id given .......addEventListner whne this happen add the click event(these are predefined) and do this
 //api----we talk to program via cli or gui..now api application programming interface......if u want to program a application do this
 //read about browser event
+
+
+
 var btnTranslate = document.querySelector('#btn-translate');
+var textInput = document.querySelector('#text-input');
 // console.log(btnTranslate)
-// btnTranslate.addEventListener("click", function clickEventHandler(){
-//     console.log("working")
-//     })
+btnTranslate.addEventListener("click", function clickEventHandler(){
+    console.log("working")
+    })
+//--another val by using call back-----  
 function clickEventHandler()
 {
 console.log("working")
-}
+console.log("input" , textInput.value);
+ }
 btnTranslate.addEventListener("click", clickEventHandler)
