ffe7857a123a2ff371067f8d504e23f5388ab09f
Ayushi Verma <50084909+Ayushi20-19@users.noreply.github.com>
2021-07-22 12:27:04 +0530

    add click event

---
+++ app.js
@@ -0,0 +1,17 @@
+// console.log("working");
+// var userName= prompt("give me username");
+// var welComeMessage = "this is working " + userName;
+// alert(welComeMessage);
+//querysector(i m asking u this thing) take the elemnt with id given .......addEventListner whne this happen add the click event(these are predefined) and do this
+//api----we talk to program via cli or gui..now api application programming interface......if u want to program a application do this
+//read about browser event
+var btnTranslate = document.querySelector('#btn-translate');
+// console.log(btnTranslate)
+// btnTranslate.addEventListener("click", function clickEventHandler(){
+//     console.log("working")
+//     })
+function clickEventHandler()
+{
+console.log("working")
+}
+btnTranslate.addEventListener("click", clickEventHandler)
