22ee6553850b8a8b449e17d5a07e6d8338e3d474
Ayushi Verma <50084909+Ayushi20-19@users.noreply.github.com>
2021-07-28 17:00:30 +0530

    api and css

--- app.js
+++ app.js
@@ -6,18 +6,35 @@
 //api----we talk to program via cli or gui..now api application programming interface......if u want to program a application do this
 //read about browser event
 
-
-
-var btnTranslate = document.querySelector('#btn-translate');
-var textInput = document.querySelector('#text-input');
+var btnTranslate = document.querySelector(".btn-translate");
+var textInput = document.querySelector("#text-input");
+var textOutput = document.querySelector("#text-output");
+var serverUrl = "https://api.funtranslations.com/translate/minion.json";
 // console.log(btnTranslate)
-btnTranslate.addEventListener("click", function clickEventHandler(){
-    console.log("working")
+// btnTranslate.addEventListener("click", function clickEventHandler(){
+//     console.log("working")
+//     })
+//--another val by using call back-----it is given as an input to another function
+
+function getTranslateHandleUrl(text) {
+  return serverUrl + "?" + "text=" + text;
+}
+
+function errorHandler(error) {
+  console.log("error occured", error);
+}
+function clickEventHandler() {
+  // console.log("working")
+  // console.log("input" , textInput.value);
+  // textOutput.innerHTML = "heyy"
+  var inputText = textInput.value; //taking input
+  //calling server for processing
+  fetch(getTranslateHandleUrl(inputText))
+    .then((response) => response.json())
+    .then((json) => {
+      var translaedText = json.contents.translated;
+      textOutput.innerHTML = translaedText;
     })
-//--another val by using call back-----  
-function clickEventHandler()
-{
-console.log("working")
-console.log("input" , textInput.value);
- }
-btnTranslate.addEventListener("click", clickEventHandler)
+    .catch(errorHandler);
+}
+btnTranslate.addEventListener("click", clickEventHandler);
